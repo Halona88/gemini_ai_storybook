c0a9d617e7376c066af534eca9e0611f9790112a
Halona <imstartupceo@gmail.com>
2025-06-19 15:49:57 +0900

    GET / HEAD / 경로 변경

--- backend/main.py
+++ backend/main.py
@@ -9,9 +9,9 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
-    allow_methods=["*"],
-    allow_headers=["*"],
+    allow_origins=["/"],
+    allow_methods=["/"],
+    allow_headers=["/"],
 )
 
 # Gemini API key 입력
